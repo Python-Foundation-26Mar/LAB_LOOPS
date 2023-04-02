911c47aad0ccd40de42f6ca8f3497e2f4f9a2efd
asmasalem1993 <129065082+asmasalem1993@users.noreply.github.com>
2023-04-02 22:08:25 +0300

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,15 @@
+#using range 
+for i in range(45, 210):
+    print(i)
+ 
+for i in range(100):
+    print(i)
+    if i >100:
+        pass
+
+#using break 
+    
+for i in range(205):
+    print(i)
+    if i >205:
+        break
---
+++ math_1.py
@@ -0,0 +1,8 @@
+
+product = " what is the product of 7 * 24? "
+while input (product) == "168":
+    print("You answered this Question correctly")
+
+else: 
+    print("Your Answer is wrong try again") 
+    print ("what is the product of 7 * 24? ")
