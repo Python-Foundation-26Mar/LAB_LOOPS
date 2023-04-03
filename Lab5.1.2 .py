4e3f166576f21a30ca263a54dfc761a5d9e15ce5
Manar Alharbi <manar.alharbi2953@gmail.com>
2023-04-03 13:08:21 +0300

    Lab5

---
+++ Lab5.1.2 .py
@@ -0,0 +1,21 @@
+from_to = range(45, 210) 
+for element in from_to:
+      print("the element is: ", element)
+      if element == 100  :
+                continue
+
+    
+      if element ==  205  :
+              break
+
+
+
+
+
+
+
+Q_A= "what is the product of 7 * 24 ?"
+while input(Q_A) != 168 :
+    print("You are wrong!, try again! ")
+else:
+    print("Correct ! you got it right ! ^_^")
