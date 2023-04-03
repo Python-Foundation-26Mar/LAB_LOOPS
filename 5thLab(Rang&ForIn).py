f271d1db59ecd8cde32d663199e624c5b96d4f8a
Maha K Alshikhi <129064826+MahaAlshikhi@users.noreply.github.com>
2023-04-04 00:00:32 +0300

    Add files via upload

---
+++ 5thLab(Rang&ForIn).py
@@ -0,0 +1,7 @@
+
+for number in range (45, 210):
+    if number == 100:
+        continue
+    elif number == 205:
+        break
+    print (number)
---
+++ 5thLab(WhileLoop).py
@@ -0,0 +1,6 @@
+ 
+question = "What is the product of 7*24?"
+while int (input (question)) != 7*24:
+    print("Your Answer is wrong try again..")
+else:
+    print("You answered this Question correctly")
