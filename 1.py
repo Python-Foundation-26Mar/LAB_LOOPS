c670c71299e9f4ad6520c23da0da18dd82673956
zakariadma <129062973+zakariadma@users.noreply.github.com>
2023-04-03 06:44:14 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+the_range = range(45,210)
+for number in the_range:
+    if number ==100:
+        continue
+    print("the number is: ",number)
+    if number == 205:
+        break
---
+++ 2.py
@@ -0,0 +1,9 @@
+Calculation=int(7*24)
+the_question="what is the product of 7 x 24 ?"
+
+while int(input(the_question)) !=Calculation:
+
+    print( "Your Answer is wrong try again..")
+
+else:
+    print("You answered this Question correctly")
